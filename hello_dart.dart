bfb189440199cb599628b730312e5398d86befa0
harshitdixit14 <113369159+harshitdixit14@users.noreply.github.com>
2024-02-11 16:12:19 +0530

    dart basics

---
+++ hello_dart.dart
@@ -0,0 +1,10 @@
+int main()
+{
+  int a=12;
+  print(a*2);
+  return 0; // this is the comment
+  /* this
+  is a multi
+  line comment */
+  
+}
---
+++ variables.dart
@@ -0,0 +1,19 @@
+int main()
+{
+  // variables in flutter are used as a container to store values
+  // there are two types of variables
+  // 1- local variable - they are declared within the function and are only accessed with the function
+  // 2- instance variable - they are declared with the class and accessible throughout the entire class
+
+  var name='Bob';
+  // variable store references 
+  // variable named name contains reference to a string object with a value Bob
+  // if object is not restricted to be a single type use "Object"
+  Object name1="harshit";
+
+  String name2="dixit";
+
+  // dart apps can't cause runtime error
+  // 
+  return 0;
+}
